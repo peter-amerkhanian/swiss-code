df7c3c706bc4b51f9d8125bf8b60cd8e83546705
Peter Amerkhanian <peteamer@gmail.com>
2025-02-12 15:23:43 -0800

    testing a package config

--- src/swiss_code/__init__.py
+++ src/swiss_code/__init__.py
@@ -1,1 +1,9 @@
-import swiss_code.data_viz as data_viz
+# src/swiss_code/__init__.py
+"""
+Swiss Code - A data visualization and geospatial analysis package.
+"""
+
+# Import submodules to expose them at the package level
+from .data_viz import geospatial, plot_types, utilities
+
+__all__ = ["geospatial", "plot_types", "utilities"]
--- src/swiss_code/data_viz/__init__.py
+++ src/swiss_code/data_viz/__init__.py
@@ -1,3 +1,10 @@
-# from swiss_code.data_viz.geospatial import *
-# from swiss_code.data_viz.plot_types import *
-# from swiss_code.data_viz.utilities import *
+# # src/swiss_code/data_viz/__init__.py
+# """
+# Swiss Code - Data visualization tools including geospatial plotting.
+# """
+
+# from .geospatial import label_polygon, label_gdf_polygons
+# from .plot_types import create_bar_chart, create_line_chart
+# from .utilities import format_labels
+
+# __all__ = ["label_polygon", "label_gdf_polygons", "create_bar_chart", "create_line_chart", "format_labels"]
